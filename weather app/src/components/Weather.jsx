13fb5c88d31c0b7638ad0a2daeb3ca2b7d4a2012
Kowshik-js <142918527+Kowshik-js@users.noreply.github.com>
2025-05-22 21:15:12 +0530

    initial commit

---
+++ weather app/src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Weather.css';
+import search_icon from '../assets/search.png';
+
+function Weather() {
+  return (
+    <div className='weather'>
+        <div className="search-bar"></div>
+        <input type="text" placeholder="search" />
+        <img src={search_icon} alt="" />
+
+    </div>
+  )
+}
+
+export default Weather
